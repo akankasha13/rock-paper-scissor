6f86d996710b188a8fa02f61679d169ff92cca3f
Akankasha <180089858+akankasha13@users.noreply.github.com>
2025-03-09 01:52:31 +0530

    RPS complete

--- script.js
+++ script.js
@@ -20,64 +20,121 @@ function getComputerChoice()
 
 //human choice//
 
-function getHumanChoice(){
-    let userInput= prompt("Enter Rock Paper or Scissor");
-    userInput= userInput.toLowerCase();
-    return userInput;
-}
+// function getHumanChoice(){
+//     let userInput= prompt("Enter Rock Paper or Scissor");
+//     userInput= userInput.toLowerCase();
+//     return userInput;
+// }
 
 //Scores//
 
 
-function playGame(){
+
     let humanScore=0
     let computerScore=0
         function playRound(humanChoice,computerChoice){
             if(humanChoice== "rock" && computerChoice== "paper"){
-                console.log("You lose :( paper wrapped the rock")
+                console.log("You lose Paper beats rock")
                 computerScore++
+                cScore.textContent= computerScore
+                result.textContent="You lose, Paper beats rock"
+
             }
             else if(humanChoice== "rock" && computerChoice== "rock"){
                 console.log("Its a tie!")
+                result.textContent="Its a tie!"
             }
 
             else if(humanChoice== "rock" && computerChoice== "scissor"){
-                console.log("You win! Rock smashed the scissor")
+                console.log("You win! Rock beats scissor")
                 humanScore++
+                hScore.textContent= humanScore
+                result.textContent="You win! Rock beats scissor"
+
             }
             else if(humanChoice== "paper" && computerChoice== "paper"){
                 console.log("Its a tie!")
+                result.textContent="Its a tie!"
             }
             else if(humanChoice== "paper" && computerChoice== "rock"){
-                console.log("You win! paper wrapped the rock")
+                console.log("You win! Paper beats rock")
                 humanScore++
+                hScore.textContent= humanScore
+                result.textContent="You win! Paper beats rock"
             }
             else if(humanChoice== "paper" && computerChoice== "scissor"){
-                console.log("You lose :( scissor cuts the paper")
+                console.log("You lose, Scissor beats paper")
                 computerScore++
+                cScore.textContent= computerScore
+                result.textContent="You lose, Scissor beats paper"
+
             }
             else if(humanChoice== "scissor" && computerChoice== "paper"){
-                console.log("You win! scissor cuts the paper")
+                console.log("You win! Scissor beats paper")
                 humanScore++
+                hScore.textContent= humanScore
+                result.textContent="You win. Scissor beats paper"
+
             }
             else if(humanChoice== "scissor" && computerChoice== "rock"){
-                console.log("You lose :( rock smashed the scissor")
+                console.log("You lose, Rock beats scissor")
                 computerScore++
+                cScore.textContent= computerScore
+                result.textContent="You lose, Rock beats scissor"
+
             }
             else if(humanChoice== "scissor" && computerChoice== "scissor"){
                 console.log("Its a tie!")
-        }
+                result.textContent="Its a tie!"
+            }
+            if(humanScore ==5 || computerScore==5){
+                rButton.disabled= true  
+                pButton.disabled= true  
+                sButton.disabled= true 
+                
+                if(humanScore > computerScore)
+                    result.textContent="You won!"
+                else if(humanScore < computerScore)
+                    result.textContent="You lost!"
 
 
+                
+           }
+           
+           
+
+
+parent.appendChild(result)
 }
-playRound(getHumanChoice(),getComputerChoice())
-playRound(getHumanChoice(),getComputerChoice())
-playRound(getHumanChoice(),getComputerChoice())
-playRound(getHumanChoice(),getComputerChoice())
-playRound(getHumanChoice(),getComputerChoice())
+
+const rButton = document.getElementById("rock")
+const pButton = document.getElementById("paper")
+const sButton = document.getElementById("scissor")
+
+rButton.addEventListener("click", () =>{
+    playRound("rock",getComputerChoice())   
+    
+})
+pButton.addEventListener("click", () =>{
+    playRound("paper",getComputerChoice())
+})
+sButton.addEventListener("click", () =>{
+    playRound("scissor",getComputerChoice())
+}) 
+
+const hScore = document.getElementById("hScore")
+const cScore = document.getElementById("cScore")
+const parent = document.getElementById("parent")
+
+const result = document.createElement("div")
+result.classList.add("result")
 
 
-}
-playGame()
+
+
+
+
+
+
 
 
